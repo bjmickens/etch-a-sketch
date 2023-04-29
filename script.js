83be805e256ba8660a2cf14abe4a29a6a10d5a84
bjmickens <128921000+bjmickens@users.noreply.github.com>
2023-04-29 17:01:55 -0500

    add DOM manipulation to new grid button

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let container = document.querySelector('.container');
-
+// create grid in the container
 function addDiv(x) {
- 
-for (let rows = 0; rows < x; rows++){
+ for (let rows = 0; rows < x; rows++){
   for (let columns = 0; columns < x; columns++){
   const newDiv = document.createElement('div'); 
  
@@ -12,8 +11,22 @@ for (let rows = 0; rows < x; rows++){
   newDiv.style.width = (600/x) + 'px';
   };
 };
-
-
 };
 
-addDiv(25);
+//remove the grid
+function removeGrid(){
+  document.querySelector('.div').remove();
+};
+
+//prompt user for number of boxes per side of grid
+function newGrid(){
+  let y = prompt("How many boxes per side?");
+  removeGrid();
+  addDiv(y);
+};
+
+let clearButton = document.querySelector('#clearButton'); 
+clearButton.addEventListener('click', newGrid);
+
+//load 16x16 grid on page load
+Window.onload = addDiv(16);
