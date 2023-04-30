d8d75e1a1a6ce948c21698a3e025809769fe1df7
bjmickens <128921000+bjmickens@users.noreply.github.com>
2023-04-29 22:22:55 -0500

    Add mouseover effect

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let container = document.querySelector('.container');
+
+
 // create grid in the container
 function addDiv(x) {
  for (let rows = 0; rows < x; rows++){
@@ -9,25 +11,38 @@ function addDiv(x) {
   newDiv.classList.add('div');
   newDiv.style.height = (600/x) + 'px';
   newDiv.style.width = (600/x) + 'px';
+  
+// add mouseover effect
+  let divs = document.querySelectorAll('.div');
+  divs.forEach((div) => {
+    div.addEventListener('mouseover', () => {
+    div.style.backgroundColor = 'black';
+  });
+});
+
+
+
   };
 };
 };
-
-//remove the grid
-// function removeGrid(){
-  
 
 
 //prompt user for number of boxes per side of new grid and remove the previous
 function newGrid(){
   let y = prompt("How many boxes per side?");
+  if (y <= 100 && y > 0){
   let newDivs = document.querySelectorAll('.div');
   newDivs.forEach((div) => { div.remove()});
   addDiv(y);
+  }
+  else {alert("Enter a number between 1-100")}
 };
 
 let clearButton = document.querySelector('#clearButton'); 
 clearButton.addEventListener('click', newGrid);
+
+
+
 
 //load 16x16 grid on page load
 Window.onload = addDiv(16);
